a99d14332805da2fae228692e2d23d6177cd7801
allan <allan.lelay@gmail.com>
2022-01-24 10:51:42 +0100

    initialize project

---
+++ item.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Assassin's Creed";
+    $price = 30;
+    $image = "https://img-3.journaldesfemmes.fr/PGBUstcnLJ37T5a8t3TWj2Obxhs=/1500x/smart/35f643214d78467e8f784125fd4e9e02/ccmcms-jdf/18344671.jpg";
+
+    echo $image;
+    echo "<br>";
+    echo $price ."<br>". $name;
+
