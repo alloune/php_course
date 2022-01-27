5d91f3bd16a4e8e46add6ca4211adf2d47da6aeb
allan <allan.lelay@gmail.com>
2022-01-27 16:57:28 +0100

    trying to get an attractive visual

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -4,7 +4,7 @@ include "head.php";
 include "my-functions.php";
 
 ?>
-    <div style="height: 1000px; position: relative; top: 200px">
+    <div class ="products">
         <?php
     $products = [
             "iPhone"=>[
@@ -30,45 +30,59 @@ include "my-functions.php";
                 "picture_url"=>"https://www.apple.com/v/imac-27/c/images/overview/display_retina__b6ivivqnixhy_large.jpg"
             ]
     ];
+
 //echo "<p>Voici le var dump des \$Products</p>";
 //var_dump($products);
 //echo"<br/><pre>";
 //print_r($products);
 //echo"</pre>";
-echo "Prix de l'iPhone :".formatPrice($products["iPhone"]["price"]);
-echo "<br>Prix de l'iPhone HT :".priceExcludingVAT($products["iPhone"]["price"]);
-echo "<br>".displayDiscountedPrice($products["iPhone"]["price"],10);
-echo "<br>----------------------------------------------";
-echo "<br>Prix de l'iPad :".formatPrice($products["iPad"]["price"]);
-echo "<br>Prix de l'iPad HT :".priceExcludingVAT($products["iPad"]["price"]);
-echo "<br>".displayDiscountedPrice($products["iPad"]["price"],0);
-echo "<br>----------------------------------------------";
-echo "<br>Prix de l'iMac :".formatPrice($products["iMac"]["price"]);
-echo "<br>Prix de l'iMac HT:".priceExcludingVAT($products["iMac"]["price"]);
-echo "<br>".displayDiscountedPrice($products["iMac"]["price"], 33);
-echo "<br>----------------------------------------------";
+
+//echo "<br>Prix de l'iPad :".formatPrice($products["iPad"]["price"]);
+//echo "<br>Prix de l'iPad HT :".priceExcludingVAT($products["iPad"]["price"]);
+//echo "<br>".displayDiscountedPrice($products["iPad"]["price"],0);
+//echo "<br>----------------------------------------------";
+//echo "<br>Prix de l'iMac :".formatPrice($products["iMac"]["price"]);
+//echo "<br>Prix de l'iMac HT:".priceExcludingVAT($products["iMac"]["price"]);
+//echo "<br>".displayDiscountedPrice($products["iMac"]["price"], 33);
+//echo "<br>----------------------------------------------";*/
 
 
 
-foreach($products as $carac =>$result){
-
-    echo "<br>";
-
-    foreach($result as $merc =>$value ){
-        if($merc === "picture_url"){
-            echo "<img class=\"photo_article\" src =".$value.">";
-
-        }
-        else {
-            echo "<br>" . $value;
-        }
-
-    }
-
-}
+//foreach($products as $carac =>$result){
+//
+//    echo "<br>";
+//
+//    foreach($result as $merc =>$value ){
+//        if($merc === "picture_url"){
+//            echo "<img class=\"photo_article\" src =".$value.">";
+//
+//        }
+//        else {
+//            echo "<br>" . $value;
+//        }
+//
+//    }
+//
+//}
         ?>
+        <div class = "iPhone">
+            <h2>Iphone</h2>
+            <img src="<?php echo $products['iPhone']['picture_url']?>">
+            <p>Iphone Blue Ciel</p>
+            <p>Prix : <?echo formatPrice($products['iPhone']['price'])?></p>
+
+
+        </div>
+        <div class = "iPad">
+            <h2>IPad</h2>
+            <img src="<?php echo $products['iPad']['picture_url']?>">
+
+        </div>
+        <div class = "iMac">
+            <h2>IMac</h2>
+            <img src="<?php echo $products['iMac']['picture_url']?>">
+        </div>
     </div>
 
-<?php
+<!---->
 
-include "footer.php";
