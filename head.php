0eecde60bcf0865ce7159c4d64842687b495dbaf
allan <allan.lelay@gmail.com>
2022-01-28 16:58:22 +0100

    forgot head

--- head.php
+++ head.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Games Bond</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="stylefranck.css">
-    <link rel="stylesheet" href="nav_footer.css">
+
+    <link rel="stylesheet" href="/nav_footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Playfair+Display&family=Roboto&family=Smooch&display=swap" rel="stylesheet">
     <link rel="icon" href="/favicon.ico"/>
+    <link rel="stylesheet" href="/stylefranck.css">
 
 
 </head>
