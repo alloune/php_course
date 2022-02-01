28347c8b78464bc110a4eab568c5e6e56103b92c
allan <allan.lelay@gmail.com>
2022-02-01 10:24:24 +0100

    upgrade it. Can handle with multi article. Should add carrier option, unworking for now

--- cart.php
+++ cart.php
@@ -10,8 +10,9 @@ foreach ($_POST as $key => $value) {
             ?>
             <div class="products">MERCI DE SÉLECTIONNER UNE QUANTITÉ VALIDE !</div>
             <?php
-            return;
+            die;
         }
+
     }
 
 }
@@ -19,9 +20,8 @@ foreach ($_POST as $key => $value) {
 
 $la_poste = array("500g" => 500, "2kg" => 0.1, "moreThan2" => 0);
 $DHL = array("500g" => 250, "5kg" => 0.15, "moreThan10" => 500);
-var_dump($_POST);
-$prices = $products[$_POST["article"]]["price"];
-$weight = $products[$_POST["article"]]["weight"];
+$totalWeight = $totalCost = 0;
+
 
 ?>
 
@@ -45,11 +45,26 @@ $weight = $products[$_POST["article"]]["weight"];
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td><?= $_POST["article"] ?></td>
-                    <td><?= $_POST["nbOfArticle"] ?></td>
-                    <td><?= formatPrice($prices) ?></td>
-                </tr>
+
+                <?php
+                foreach ($_POST as $data) {
+
+                    foreach ($data as $key => $value) {
+                        if ($value['quantity']) {
+                            ?>
+                            <tr>
+                            <td><?= $key ?></td>
+                            <td><?= $value['quantity'] ?></td>
+                            <td><?= formatPrice($products[$key]["price"]) ?></td>
+                            </tr><?php
+
+                            $totalCost = $totalCost + $value['quantity'] * $products[$key]["price"];
+                            $totalWeight = $totalWeight + $value['quantity'] * $products[$key]["weight"];
+                        }
+                    }
+
+                }
+                ?>
                 </tbody>
 
             </table>
@@ -58,11 +73,10 @@ $weight = $products[$_POST["article"]]["weight"];
             <div>
                 <h3>Prix</h3>
 
-                <p><br><br>Pour <?= $_POST["nbOfArticle"] . " " . $_POST["article"] ?> le cout total est de
-                    : <?= formatPrice($total = ($_POST["nbOfArticle"] * $prices)) ?> </p>
+                <p><br><br>Le cout total est de : <?= formatPrice($totalCost) ?> </p>
                 <p>Vous bénéficiez exceptionnellement d'une remise de <?= ($remise = 25) . "%"; ?></p>
                 <p class="finalPrice">Prix après reduction
-                    : <?= formatPrice($totalDiscounted = displayDiscountedPrice(intval($total), $remise)); ?></p>
+                    : <?= formatPrice($totalDiscounted = displayDiscountedPrice(intval($totalCost), $remise)); ?></p>
                 <p class="htPrice">Prix HT : <?= formatPrice(priceExcludingVAT(intval($totalDiscounted))) ?></p>
             </div>
             <form class="shipmentMethod" method="post" action="cart.php">
@@ -80,7 +94,7 @@ $weight = $products[$_POST["article"]]["weight"];
                 </div>
                 <div class="shipmentCost">
 
-                    <?php $totalWeight = ($weight * $_POST["nbOfArticle"]) ?>
+
                     <p>Le poids du colis est de : <?= formatWeight($totalWeight) ?> </p>
                     <input type="submit" value="Confirmer">
 
