72b43ccd6a198bf4f58cb5c1a3a7f375c8b13158
allan <allan.lelay@gmail.com>
2022-02-21 11:29:41 +0100

    new way, POO

--- database.php
+++ database.php
@@ -1,47 +1,51 @@
 <?php
 try {
-    $mysqlConnection = new PDO(
+     $mysqlConnection = new PDO(
         'mysql:host=localhost;dbname=sellsdb;charset=utf8',
         'allan',
         'Chacha38'
-
     );
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
 
+// REORGANISATION A FAIRE
 //SELECT ALL PRODUCTS
+function getAllProduct(){
+    $selectAllProducts = $GLOBALS['mysqlConnection']->query('SELECT * FROM `products`');
+    $productsList = $selectAllProducts->fetchAll();
+    return $productsList;
 
-$selectAllProducts = $mysqlConnection->query('SELECT * FROM `products`');
-$productsList = $selectAllProducts->fetchAll();
+}
 
-//SELECT ALL CUSTOMERS
 
-$selectAllCust = $mysqlConnection->query('SELECT first_name, last_name FROM customers');
-$custList = $selectAllCust->fetchAll();
+////SELECT ALL CUSTOMERS
+//
+//$selectAllCust = $mysqlConnection->query('SELECT first_name, last_name FROM customers');
+//$custList = $selectAllCust->fetchAll();
 
 //VIEW ORDER COST
 
-$allOrdersCost = $mysqlConnection->query(
-    'SELECT number, sum(order_product.quantity*price) as Total_Price
-FROM `order_product`
-          JOIN products ON products.id = product_id
-          JOIN orders ON orders.id = order_id
-GROUP BY number');
-
-$ordersCost = $allOrdersCost->fetchAll();
-
-//View order between 100 and 500 €
-$costbt100n500 = $mysqlConnection->query('SELECT number, SUM(price *order_product.quantity ) as Total
-FROM order_product
-         INNER JOIN orders ON orders.id = order_id
-         INNER JOIN products ON products.id = product_id
-WHERE Total BETWEEN 100 AND 500
-GROUP BY number');
-$finalSort = $costbt100n500 ->fetchAll();
-
-
-$update = $mysqlConnection->query("UPDATE products SET price = 2500, avaible = 1 WHERE price = 3"  );
+//$allOrdersCost = $mysqlConnection->query(
+//    'SELECT number, sum(order_product.quantity*price) as Total_Price
+//FROM `order_product`
+//          JOIN products ON products.id = product_id
+//          JOIN orders ON orders.id = order_id
+//GROUP BY number');
+//
+//$ordersCost = $allOrdersCost->fetchAll();
+//
+////View order between 100 and 500 €
+//$costbt100n500 = $mysqlConnection->query('SELECT number, SUM(price *order_product.quantity ) as Total
+//FROM order_product
+//         INNER JOIN orders ON orders.id = order_id
+//         INNER JOIN products ON products.id = product_id
+//WHERE Total BETWEEN 100 AND 500
+//GROUP BY number');
+//$finalSort = $costbt100n500 ->fetchAll();
+//
+//
+//$update = $mysqlConnection->query("UPDATE products SET price = 2500, avaible = 1 WHERE price = 3"  );
 
 
 
--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -8,66 +8,9 @@ include "database.php";
 include "head.php";
 include "my-functions.php";
 include "catalog.php";
-
+$productsList=getAllProduct();
 ?>
 
-<!--<form method="post">-->
-<!--<div class="test">-->
-<!--    <h2>Test de session</h2>-->
-<!--    --><?php //if (isset($_SESSION['name']) && $_SESSION['name'] != "") {
-//        echo "Bonjour " . $_SESSION['name'] . ", bievenue sur notre site !";
-//
-//    } ?>
-<!--    <p>Quel est votre nom ?</p>-->
-<!--    <input type="text" name="name">-->
-<!--    <input type="submit">-->
-<!--</form>-->
-<!---->
-<!--</div>-->
-<div class="productsList">
-    <ul>
-        <?php
-
-        foreach ($productsList as $product) {
-            ?>
-            <li class=productsLine>
-                <form action="delete.php" method="post">
-                    <img class="productimg" src="<?= $product['image'] ?>" alt="Image de : <?= $product['name'] ?>">
-                    <h2><?= $product['name'] ?></h2>
-                    <h3><?= $product['price'] ?> €</h3>
-                    <input type="hidden" name="id" value="<?= $product['id'] ?>">
-                    <input class="deletebtn" type="submit" value="Supprimer l'article">
-                </form>
-
-            </li>
-            <?php
-
-
-        }
-
-        ?>
-    </ul>
-</div>
-<div class=addProduct>
-    <h1>AJOUT D'UN NOUVEAU PRODUIT</h1>
-    <form method="post" action="insert.php">
-        <input type="text" name="name" placeholder="nom du produit">
-        <input type="text" name="price" placeholder="prix du produit">
-        <input type="text" name="weight" placeholder="poids unitaire">
-        <input type="radio" name="avaible" id="oui" value="1">
-        <label for="oui">Oui</label>
-        <input type="radio" name="avaible" id="non" value="0">
-        <label for="non">Non</label>
-        <input type="text" name="description" placeholder="description">
-        <input type="text" name="image" placeholder="Lien de l'image">
-        <input type="number" name="quantity" placeholder="Quantité disponible">
-        <input type="submit" value="Ajouter mon produit">
-
-
-    </form>
-
-
-</div>
 
 
 
