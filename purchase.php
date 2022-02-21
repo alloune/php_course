25d2774079803bdf01b08d86b491d94d8f1a1e76
allan <allan.lelay@gmail.com>
2022-02-21 16:26:21 +0100

    changing branch

--- purchase.php
+++ purchase.php
@@ -5,57 +5,19 @@ include "database.php";
 include "head.php";
 include "my-functions.php";
 include "catalog.php";
-$listOfThings = getAllProduct();
-
-
-class Item
-{
-    public string $name;
-    public string $description;
-    public int $price;
-    public string $imageURL;
-    public int $weight;
-    public int $stock;
-    public bool $available;
-    public int $quantity;
-    public int $category;
-
-}
-
-
-
-//var_dump($listOfThings);
-
-function displayItem(Item $product)
-{
-
-    ?>
-    <div class="productsList">
-        <p><?= $product->name ?></p>
-        <p><?= $product->price ."€" ?></p>
-        <img src="<?= $product->imageURL ?>">
-
-    </div>
-<?php
-
-}
+include "class.php";
 
 ?>
-<div>
+<div class="test">
+
+    <ul class="pooList">
+        <?php
+        $catalogue = new Catalogue($mysqlConnection);
+        echo displayCataloque($catalogue);
 
 
-    <?php
-
-    foreach ($listOfThings as $product){
-        $test = new Item();
-        $test->name=$product["name"];
-        $test->price=$product["price"];
-        $test->imageURL=$product["image"];
-        displayItem($test);
-    }
-
-
-    ?>
+        ?>
+    </ul>
 </div>
 
 
